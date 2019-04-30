f5d967e65e514af7822350a105c544c909db0feb
rocky <rockysynergy@qq.com>
2019-04-30 13:47:18 +0800

    updated pull

---
+++ pull.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+cd /mnt/c/Users/rocky/Documents/Personal/Blog/rockysynergy.github.io/
+git pull
+rsync -av /mnt/c/Users/rocky/Documents/Personal/Blog/rockysynergy.github.io/input/ /mnt/c/xampp/htdocs/Yelly/input/
